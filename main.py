32150a4e6146545b28ef47d7f6d9b9a5d9e457dd
madsnesse <45721557+madsnesse@users.noreply.github.com>
2021-05-04 11:10:33 +0200

    test

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+
+
+client = discord.Client()
+TOKEN = ""
+with open("token.txt", "r") as f:
+    TOKEN = f.read()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
